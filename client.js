4628b13bc8572b590c821a603cfd4499b84708a6
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-05-20 23:55:11 +0200

    Refactored client.
    Now the store API part is splitted into its own facette
    matching the store interface.

--- client.js
+++ client.js
@@ -3,350 +3,353 @@
 // Substance.Client is freely distributable under the MIT license.
 // For all details and documentation:
 // http://github.com/substance/client
-
 (function() {
-  if (!window.Substance) window.Substance = {};
 
-  // TODO: can't handle paths including a query string!
-  function _generic_request(host, method, path, data, headers, cb, raw) {
+if (!window.Substance) window.Substance = {};
 
-    // TODO: Move to util?
-    function toQueryString(obj) {
-      var str = [];
-      for(var p in obj)
-         str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-      return str.join("&");
-    }
+// TODO: can't handle paths including a query string!
+function _generic_request(host, method, path, data, headers, cb, raw) {
 
-    function getURL() {
-      var url = host + path;
-      if (method.toUpperCase() === "GET" && data && Object.keys(data).length > 0) {
-        url += "?"+toQueryString(data);
-      }
-      return url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime();
-    }
-
-    var xhr = new XMLHttpRequest();
-    if (!raw) {xhr.dataType = "json";}
-
-    xhr.open(method, getURL());
-    xhr.onreadystatechange = function () {
-      if (this.readyState == 4) {
-        // TODO: this needs some explanation
-        if (this.status >= 200 && this.status < 300 || this.status === 304) {
-          cb(null, raw ? this.responseText : this.responseText ? JSON.parse(this.responseText) : true);
-        } else {
-          var err;
-          // try to interpret the response as json
-          try {
-            var err = JSON.parse(this.responseText);
-            if (err.stack) console.log(err.stack);
-          } catch (err) {
-            // if not possible fall back to string based errors
-            err = this.responseText;
-          }
-          cb(err);
-        }
-      }
-    };
-
-    // xhr.setRequestHeader('Accept','application/vnd.github.raw');
-    // xhr.setRequestHeader('Content-Type','application/json');
-
-    //HACK: because DELETE doesnt accept application/json content type
-    if (method.toUpperCase() !== 'DELETE') {
-      headers['Content-Type'] = "application/json";
-    }
-
-    _.each(headers, function(value, key) {
-      xhr.setRequestHeader(key, value);
-    });
-
-    data ? xhr.send(JSON.stringify(data)) : xhr.send();
+  // TODO: Move to util?
+  function toQueryString(obj) {
+    var str = [];
+    for(var p in obj)
+       str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    return str.join("&");
   }
 
+  function getURL() {
+    var url = host + path;
+    if (method.toUpperCase() === "GET" && data && Object.keys(data).length > 0) {
+      url += "?"+toQueryString(data);
+    }
+    return url + ((/\?/).test(url) ? "&" : "?") + (new Date()).getTime();
+  }
 
-  // Substance.Client Interface
+  var xhr = new XMLHttpRequest();
+  if (!raw) {xhr.dataType = "json";}
+
+  xhr.open(method, getURL());
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4) {
+      // TODO: this needs some explanation
+      if (this.status >= 200 && this.status < 300 || this.status === 304) {
+        cb(null, raw ? this.responseText : this.responseText ? JSON.parse(this.responseText) : true);
+      } else {
+        var err;
+        // try to interpret the response as json
+        try {
+          var err = JSON.parse(this.responseText);
+          if (err.stack) console.log(err.stack);
+        } catch (err) {
+          // if not possible fall back to string based errors
+          err = this.responseText;
+        }
+        cb(err);
+      }
+    }
+  };
+
+  // xhr.setRequestHeader('Accept','application/vnd.github.raw');
+  // xhr.setRequestHeader('Content-Type','application/json');
+
+  //HACK: because DELETE doesnt accept application/json content type
+  if (method.toUpperCase() !== 'DELETE') {
+    headers['Content-Type'] = "application/json";
+  }
+
+  _.each(headers, function(value, key) {
+    xhr.setRequestHeader(key, value);
+  });
+
+  data ? xhr.send(JSON.stringify(data)) : xhr.send();
+}
+
+
+// Substance.Client Interface
+// -------
+
+var Client = function(options) {
+  var that = this;
+
+  if (options.token) this.token = options.token;
+
+  var private = new Client_private(this, options);
+
+  // Helpers
+  // =======
+
+  this.request = function(method, path, data, cb, raw) {
+    return private._request(method, path, data, private.headers(), cb, raw);
+  };
+
+  // Users API
+  // ==========================
+
+  // Authenticate with username and password
   // -------
 
-  Substance.Client = function(options) {
-    var that = this;
+  this.authenticate = function(username, password, cb) {
+    var self = this;
 
-    if (options.token) this.token = options.token;
-
-    // Helpers
-    // =======
-
-    function headers() {
-      var headers = {};
-      // Adds authorization token if available
-      if (that.token) {
-        headers["Authorization"] = "token " + that.token;
-      }
-      return headers;
-    }
-
-    function _request(method, path, data, headers, cb, raw) {
-      return _generic_request(options.hub_api, method, path, data, headers, cb, raw);
-    }
-
-    this.request = function(method, path, data, cb, raw) {
-      return _request(method, path, data, headers(), cb, raw);
+    var data = {
+      "client_id": options.client_id,
+      "client_secret": options.client_secret
     };
 
-
-    // Seed
-    // ==========================
-
-    this.seed = function(seed_or_seedName, cb) {
-      if (Object.prototype.toString.call(seed_or_seedName) === '[object String]') {
-       var seedName = seed_or_seedName;
-       console.log("Seeding hub with", seedName, "...");
-        this.request("GET", "/seed/"+seedName, null, function(err, res) {
-          if(err) console.log("...failed", err);
-          else console.log("...done")
-          cb(err, res);
-        });
-      } else {
-        var seed = seed_or_seedName;
-        console.log("Seeding hub with object", seed, "...");
-        this.request("POST", "/seed", seed, function(err, res) {
-          cb(err, res);
-        });
-
-      }
+    var headers = {
+      'Authorization': 'Basic ' + Base64.encode(username + ':' + password)
     };
 
-    // Users API
-    // ==========================
-
-    // Authenticate with username and password
-    // -------
-
-    this.authenticate = function(username, password, cb) {
-      var data = {
-        "client_id": options.client_id,
-        "client_secret": options.client_secret
-      };
-
-      var headers = {
-        'Authorization': 'Basic ' + Base64.encode(username + ':' + password)
-      };
-
-      _request("POST", "/authorizations", data, headers, function(err, data) {
-        if (err) return cb(err);
-        that.token = data.token;
-        cb(null, data);
-      });
-    };
-
-
-    // Create new publication on the server
-    // -------
-
-    this.createUser = function(user, cb) {
-      var data = {
-        "username": user.username,
-        "email": user.email,
-        "name": user.name,
-        "password": user.password,
-        "client_id": options.client_id,
-        "client_secret": options.client_secret
-      };
-
-      this.request("POST", "/register", data, function(err, res) {
-        cb(err, res);
-      });
-    };
-
-
-
-    // Document API
-    // ==========================
-
-
-    // Create a new document
-    // ----------
-
-    this.createDocument = function(id, options, cb) {
-      this.request('POST', '/documents', _.extend(options, {id: id}), function(err, doc) {
-        cb(err, doc);
-      });
-    };
-
-    // Get document by id
-    // ----------
-
-    this.getDocument = function(id, cb) {
-      this.request('GET', '/documents/'+id, null, function(err, doc) {
-        cb(err, doc);
-      });
-    };
-
-    this.getDocumentInfo = function(id, cb) {
-      this.request('GET', '/documents/'+id+'/info', null, function(err, info) {
-        cb(err, info);
-      });
-    };
-
-    // List all allowed documents complete with metadata
-    // -------
-
-    // TODO: Currently the hub returns a hash for documents, should be a list!
-    this.listDocuments = function (cb) {
-      this.request('GET', '/documents', null, cb);
-    };
-
-    // Permanently deletes a document
-    // -------
-
-    this.deleteDocument = function (id, cb) {
-      this.request("DELETE", '/documents/' + id, null, function(err) {
-        cb(err);
-      });
-    };
-
-    // Retrieves a range of the document's commits
-    // -------
-
-    this.documentCommits = function(id, last, since, cb) {
-      // Head defaults to tail on the server, we should make this explicit
-      // Provide head to server!
-      this.request("GET", '/documents/'+id+'/commits', {since: since, last: last}, function(err, commits) {
-        cb(err, commits);
-      });
-    };
-
-    // Stores a sequence of commits for a given document id.
-    // -------
-
-    // TODO: update original API so they also take meta and refs
-    this.updateDocument = function(id, options, cb) {
-      this.request("PUT", '/documents/'+id, options, function (err) {
-        return cb(err);
-      });
-    };
-
-    // Create Blob on the server
-    // -------
-
-    this.createBlob = function(docId, blobId, blobData, cb) {
-      this.request("POST", '/documents/'+docId+'/blobs/'+blobId, {data: blobData}, function (err) {
-        return cb(err);
-      });
-    };
-
-    // Get Blob from server
-    // -------
-
-    this.getBlob = function(docId, blobId, cb) {
-      this.request("GET", '/documents/'+docId+'/blobs/'+blobId, null, cb);
-    };
-
-    this.deleteBlob = function(docId, blobId, cb) {
-      this.request("DELETE", '/documents/'+docId+'/blobs/'+blobId, null, cb);
-    };
-
-    this.listBlobs = function(docId, cb) {
-      this.request("GET", '/documents/'+docId+'/blobs', null, cb);
-    };
-
-    // Publications API
-    // ==========================
-
-    // Create new publication on the server
-    // -------
-
-    this.createPublication = function(document, network, cb) {
-      this.request("POST", "/publications", {document: document, network: network}, function(err, res) {
-        cb(err, res);
-      });
-    };
-
-    // Delete publication from the server
-    // -------
-
-    this.deletePublication = function(id, cb) {
-      this.request("DELETE", "/publications/"+id, null, function(err, res) {
-        cb(err, res);
-      });
-    };
-
-
-    // List Publications for a document
-    // -------
-
-    this.listPublications = function(document, cb) {
-      this.request("GET", "/publications", {document: document}, function(err, publications) {
-        cb(err, publications);
-      });
-    };
-
-
-    // Networks API
-    // ==========================
-
-    // Get all available networks
-    // -------
-
-    this.listNetworks = function(cb) {
-      this.request("GET", "/networks", null, function(err, networks) {
-        cb(err, networks);
-      });
-    };
-
-
-    // Versions API
-    // ==========================
-
-
-    // Create new version for a document
-    // -------
-
-    this.createVersion = function(document, data, cb) {
-      this.request("POST", "/versions", {document: document, data: JSON.stringify(data)}, function(err, res) {
-        cb(err, res);
-      });
-    };
-
-    // Delete all versions for a document
-    // -------
-
-    this.unpublish = function(document, cb) {
-      this.request("DELETE", "/versions", {document: document}, function(err) {
-        cb(err);
-      });
-    };
-
-    // Collaborators API
-    // ==========================
-
-    // List collaborators for a document
-    // -------
-
-    this.listCollaborators = function(document, cb) {
-      this.request("GET", "/collaborators", {document: document}, function(err, res) {
-        cb(err, res);
-      });
-    };
-
-    // Create collaborator for document
-    // -------
-
-    this.createCollaborator = function(document, collaborator, cb) {
-      this.request("POST", "/collaborators", {collaborator: collaborator, document: document}, function(err, res) {
-        cb(err, res);
-      });
-    };
-
-    // Delete collaborator for a document
-    // -------
-
-    this.deleteCollaborator = function(collaborator, cb) {
-      this.request("DELETE", "/collaborators/"+collaborator, null, function(err, res) {
-        cb(err, res);
-      });
-    };
-
+    private._request("POST", "/authorizations", data, headers, function(err, data) {
+      if (err) return cb(err);
+      self.token = data.token;
+      cb(null, data);
+    });
   };
+
+  // Create new publication on the server
+  // -------
+
+  this.create = function(user, cb) {
+    var data = {
+      "username": user.username,
+      "email": user.email,
+      "name": user.name,
+      "password": user.password,
+      "client_id": options.client_id,
+      "client_secret": options.client_secret
+    };
+
+    this.request("POST", "/register", data, function(err, res) {
+      cb(err, res);
+    });
+  };
+
+  // Publications API
+  // ==========================
+
+  // Create new publication on the server
+  // -------
+
+  this.createPublication = function(document, network, cb) {
+    this.request("POST", "/publications", {document: document, network: network}, function(err, res) {
+      cb(err, res);
+    });
+  };
+
+  // Delete publication from the server
+  // -------
+
+  this.deletePublication = function(id, cb) {
+    this.request("DELETE", "/publications/"+id, null, function(err, res) {
+      cb(err, res);
+    });
+  };
+
+
+  // List Publications for a document
+  // -------
+
+  this.listPublications = function(document, cb) {
+    this.request("GET", "/publications", {document: document}, function(err, publications) {
+      cb(err, publications);
+    });
+  };
+
+
+  // Networks API
+  // ==========================
+
+  // Get all available networks
+  // -------
+
+  this.listNetworks = function(cb) {
+    this.request("GET", "/networks", null, function(err, networks) {
+      cb(err, networks);
+    });
+  };
+
+
+  // Versions API
+  // ==========================
+
+
+  // Create new version for a document
+  // -------
+
+  this.createVersion = function(document, data, cb) {
+    this.request("POST", "/versions", {document: document, data: JSON.stringify(data)}, function(err, res) {
+      cb(err, res);
+    });
+  };
+
+  // Delete all versions for a document
+  // -------
+
+  this.unpublish = function(document, cb) {
+    this.request("DELETE", "/versions", {document: document}, function(err) {
+      cb(err);
+    });
+  };
+
+  // Collaborators API
+  // ==========================
+
+  // List collaborators for a document
+  // -------
+
+  this.listCollaborators = function(document, cb) {
+    this.request("GET", "/collaborators", {document: document}, function(err, res) {
+      cb(err, res);
+    });
+  };
+
+  // Create collaborator for document
+  // -------
+
+  this.createCollaborator = function(document, collaborator, cb) {
+    this.request("POST", "/collaborators", {collaborator: collaborator, document: document}, function(err, res) {
+      cb(err, res);
+    });
+  };
+
+  // Delete collaborator for a document
+  // -------
+
+  this.deleteCollaborator = function(collaborator, cb) {
+    this.request("DELETE", "/collaborators/"+collaborator, null, function(err, res) {
+      cb(err, res);
+    });
+  };
+
+  // Store API
+  // =========
+  // using a dedicated scope for the store
+
+  this.store = new Client.Store(this);
+};
+
+var Client_private = function(self, options) {
+
+  this.headers = function() {
+    var headers = {};
+    // Adds authorization token if available
+    if (self.token) {
+      headers["Authorization"] = "token " + self.token;
+    }
+    return headers;
+  }
+
+  this._request = function(method, path, data, headers, cb, raw) {
+    return _generic_request(options.hub_api, method, path, data, headers, cb, raw);
+  }
+
+}
+
+Client.Store = function(client) {
+
+  // Seed
+  // ==========================
+
+  this.seed = function(seed_or_seedName, cb) {
+    if (Object.prototype.toString.call(seed_or_seedName) === '[object String]') {
+     var seedName = seed_or_seedName;
+     console.log("Seeding hub with", seedName, "...");
+      client.request("GET", "/seed/"+seedName, null, function(err, res) {
+        if(err) console.log("...failed", err);
+        else console.log("...done")
+        cb(err, res);
+      });
+    } else {
+      var seed = seed_or_seedName;
+      console.log("Seeding hub with object", seed, "...");
+      client.request("POST", "/seed", seed, function(err, res) {
+        cb(err, res);
+      });
+
+    }
+  };
+
+  this.create = function(id, options, cb) {
+    client.request('POST', '/documents', _.extend(options, {id: id}), function(err, doc) {
+      cb(err, doc);
+    });
+  };
+
+  this.get = function(id, cb) {
+    client.request('GET', '/documents/'+id, null, function(err, doc) {
+      cb(err, doc);
+    });
+  };
+
+  this.getInfo = function(id, cb) {
+    client.request('GET', '/documents/'+id+'/info', null, function(err, info) {
+      cb(err, info);
+    });
+  };
+
+  // TODO: Currently the hub returns a hash for documents, should be a list!
+  this.list = function (cb) {
+    client.request('GET', '/documents', null, cb);
+  };
+
+  this.delete = function (id, cb) {
+    client.request("DELETE", '/documents/' + id, null, function(err) {
+      cb(err);
+    });
+  };
+
+  // Retrieves a range of the document's commits
+  // -------
+  //
+  this.commits = function(id, options, cb) {
+    // Head defaults to tail on the server, we should make this explicit
+    // Provide head to server!
+    client.request("GET", '/documents/'+id+'/commits', options, function(err, commits) {
+      cb(err, commits);
+    });
+  };
+
+  this.update = function(id, options, cb) {
+    client.request("PUT", '/documents/'+id, options, function (err) {
+      return cb(err);
+    });
+  };
+
+  this.blobs = new Client.Store.Blobs(client);
+
+};
+
+Client.Store.Blobs = function(client) {
+
+  // Create Blob on the server
+  // -------
+
+  this.create = function(docId, blobId, blobData, cb) {
+    client.request("POST", '/documents/'+docId+'/blobs/'+blobId, {data: blobData}, function (err) {
+      return cb(err);
+    });
+  };
+
+  // Get Blob from server
+  // -------
+
+  this.get = function(docId, blobId, cb) {
+    client.request("GET", '/documents/'+docId+'/blobs/'+blobId, null, cb);
+  };
+
+  this.delete = function(docId, blobId, cb) {
+    client.request("DELETE", '/documents/'+docId+'/blobs/'+blobId, null, cb);
+  };
+
+  this.list = function(docId, cb) {
+    client.request("GET", '/documents/'+docId+'/blobs', null, cb);
+  };
+
+};
+
+
+window.Substance.Client = Client;
+
 }).call(this);
